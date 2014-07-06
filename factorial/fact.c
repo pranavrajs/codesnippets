5afc95377f2c07236e08d4e9c71d136e1de459c8
pranavrajs <pranvrajs@gmail.com>
2014-07-06 08:52:40 +0530

    Faster algorithm for factorial

---
+++ factorial/fact.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+
+	long long unsigned int num;
+	scanf("%llu",&num);
+	long long unsigned int prod,subs,temp;
+
+	if(num==1)
+	{
+		printf("1");
+		return 0;
+	}
+	/*
+		For an even number say 8
+		8! = 8*7*6*5*4*3*2*1
+		which is equal to
+		8! = 8*(7*2)*(6*3)*(5*4)
+
+		8! = 8*(8+6)*(14+4)*(18+2)
+
+		For an odd number say 7
+		7! = 7*6*5*4*3*2*1
+		which is equal to
+		7! = 7*(6*2)*(5*3)*4
+		7! = 7*(7+5)*(12+3)*ceil(7/2)
+
+	*/
+	subs=num-2;
+	prod=num;
+	temp=prod;
+	while(subs>=2)
+	{
+		temp=temp+subs;
+		prod*=temp;
+		subs-=2;
+	}
+	/* If number is odd multiply it with roundUp(num/2) */
+	if(num%2!=0)
+		prod*=(num/2+1);
+
+	printf("%llu",prod);
+}
